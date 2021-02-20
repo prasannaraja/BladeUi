ed0dd6c73bc4de38ca78749cdf23e8150eb4ee2d
Prasanna Raja <prasannaraja@msn.com>
2021-02-20 23:39:18 +0530

    Delete base-list-blade.directive.ts

--- src/app/shared/blader/base-balde/base-list-blade.directive.ts
+++
@@ -1,8 +0,0 @@
-import { Directive, OnInit } from "@angular/core";
-import { BaseBladeDirective } from "./base-blade.directive";
-
-@Directive()
-export abstract class BaseListBladeDirective<T> extends BaseBladeDirective 
-implements OnInit{
-
-}
