37a9aa70b1ad342fdc08277fa23418e93ae26196
Prasanna Raja <prasannaraja@msn.com>
2021-02-20 23:59:41 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
-import { BrowserModule } from '@angular/platform-browser';
+
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { SharedModule } from './shared.module';
 import { HttpClientModule } from '@angular/common/http';
+import { SharedModule } from "./shared.module";
 
 @NgModule({
   declarations: [
