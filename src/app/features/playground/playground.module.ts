0c8715f5f24d0c0b1391c205907c86c9eb0b103e
Prasanna Raja <prasannaraja@msn.com>
2021-02-21 01:44:15 +0530

    playground updated

--- src/app/features/playground/playground.module.ts
+++ src/app/features/playground/playground.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PlaygroundComponent } from './playground.component';
 import { PlaygroundRoutingModule } from './playground-routing.module';
+import { MaterialModules } from 'src/app/material-modules';
 
 @NgModule({
   declarations: [PlaygroundComponent],
-  exports:[PlaygroundComponent],
-  imports: [
-    CommonModule,
-    PlaygroundRoutingModule
-  ]
+  exports: [PlaygroundComponent],
+  imports: [CommonModule, MaterialModules, PlaygroundRoutingModule],
 })
 export class PlaygroundModule { }
